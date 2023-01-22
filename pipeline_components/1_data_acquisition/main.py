88c8a54cfdb35be0bdea45aea22e824a47b36803
Ali <azamani1@ualberta.ca>
2023-01-22 00:03:18 -0700

    Add parse arguments and load yaml config

--- pipeline_components/1_data_acquisition/main.py
+++ pipeline_components/1_data_acquisition/main.py
@@ -2,10 +2,12 @@
 The file should implement data acquisition pipeline.
 It should load the raw data from any data source and save it.
 """
+from typing import List
+
 import pandas as pd
 from sklearn import datasets
 
-from src.utils import write_parquet
+from src.utils import load_yaml_config, parse_arguments, write_parquet
 
 
 def load_data() -> pd.DataFrame:
@@ -19,10 +21,13 @@ def load_data() -> pd.DataFrame:
     return iris_dataset_df
 
 
-def main():
-    """Run method"""
+def main(arguments_list: List = None):
+    """Main method"""
+    arguments = parse_arguments(arguments_list=arguments_list)
+    config = load_yaml_config(arguments.config_path)
+    main_config = load_yaml_config(arguments.main_config_path)
     dataset_df = load_data()
-    write_parquet(df=dataset_df, path='data/dataset.parquet')
+    write_parquet(df=dataset_df, path=config.get('output_data_path'))
 
 
 if __name__ == '__main__':
