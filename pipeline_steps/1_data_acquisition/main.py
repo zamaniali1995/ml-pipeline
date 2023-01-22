dbff1b7c73d3b105752e6c09e364dec9c70607df
Ali <azamani1@ualberta.ca>
2023-01-21 18:57:58 -0700

    Add load_data method

--- pipeline_steps/1_data_acquisition/main.py
+++ pipeline_steps/1_data_acquisition/main.py
@@ -6,14 +6,20 @@ import pandas as pd
 from sklearn import datasets
 
 
-def run():
-    """Run method"""
+def load_data() -> pd.DataFrame:
+    """Complete me"""
     iris_dataset = datasets.load_iris()
     X = iris_dataset.data
     y = iris_dataset.target
     y_df = pd.DataFrame(y, columns=['target'])
     X_df = pd.DataFrame(X, columns=iris_dataset.feature_names)
     iris_dataset_df = pd.concat([X_df, y_df], axis=1, join='inner')
+    return iris_dataset_df
+
+
+def run():
+    """Run method"""
+    dataset = load_data()
 
 
 if __name__ == '__main__':
