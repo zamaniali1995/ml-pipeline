ce987d6f5373407bbbf5d8ac3387a10710573084
Ali <azamani1@ualberta.ca>
2023-01-21 22:41:40 -0700

    Change pipeline_steps to pipeline_components

---
+++ pipeline_components/1_data_acquisition/main.py
@@ -0,0 +1,29 @@
+"""
+The file should implement data acquisition pipeline.
+It should load the raw data from any data source and save it.
+"""
+import pandas as pd
+from sklearn import datasets
+
+from src.utils import write_parquet
+
+
+def load_data() -> pd.DataFrame:
+    """Complete me"""
+    iris_dataset = datasets.load_iris()
+    X = iris_dataset.data
+    y = iris_dataset.target
+    y_df = pd.DataFrame(y, columns=['target'])
+    X_df = pd.DataFrame(X, columns=iris_dataset.feature_names)
+    iris_dataset_df = pd.concat([X_df, y_df], axis=1, join='inner')
+    return iris_dataset_df
+
+
+def main():
+    """Run method"""
+    dataset_df = load_data()
+    write_parquet(df=dataset_df, path='data/dataset.parquet')
+
+
+if __name__ == '__main__':
+    main()
