3b5df90f9c6f5585e35a87c0934817cbeaa86365
Ali <azamani1@ualberta.ca>
2023-01-22 00:04:17 -0700

    Add parse_arguments method

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,13 @@
 Complete me
 """
 import os
+from argparse import ArgumentParser, Namespace
 from pathlib import Path
-from typing import Text
+from typing import List, Text
 
 import pandas as pd
+import yaml
+from yaml.loader import SafeLoader
 
 
 def write_parquet(df: pd.DataFrame, path: Path, engine: Text = 'pyarrow'):
@@ -14,3 +17,20 @@ def write_parquet(df: pd.DataFrame, path: Path, engine: Text = 'pyarrow'):
     dir = Path(os.path.split(path)[0])
     dir.mkdir(parents=True, exist_ok=True)
     df.to_parquet(path=path, engine=engine)
+
+
+def parse_arguments(arguments_list: List) -> Namespace:
+    """Complete me
+    """
+    parser = ArgumentParser('Parse config path.')
+    parser.add_argument(
+        '--config_path',
+        type=Path,
+        help='Path to the config path.'
+    )
+    parser.add_argument(
+        '--main_config_path',
+        type=Path,
+        help='Path to the main config path.'
+    )
+    return parser.parse_args(arguments_list)
