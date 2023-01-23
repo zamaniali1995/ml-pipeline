74c9a300e95983185490f877f1210415782cc9ee
Ali <azamani1@ualberta.ca>
2023-01-23 15:32:54 -0700

    Update model_training/config.yaml

--- pipeline_components/1_data_acquisition/main.py
+++ pipeline_components/1_data_acquisition/main.py
@@ -7,7 +7,7 @@ from typing import List
 import pandas as pd
 from sklearn import datasets
 
-from src.utils import load_yaml_config, parse_arguments, write_parquet
+from src.utils import Log, load_yaml_config, parse_arguments, write_parquet
 
 
 def load_data() -> pd.DataFrame:
@@ -26,8 +26,18 @@ def main(arguments_list: List = None):
     arguments = parse_arguments(arguments_list=arguments_list)
     config = load_yaml_config(arguments.config_path)
     main_config = load_yaml_config(arguments.main_config_path)
+
+    Log.info(main_config.log_msg.acquire_data_start_msg)
+
+    Log.info(main_config.log_msg.load_dataset_start_msg)
     dataset_df = load_data()
+    Log.info(main_config.log_msg.load_dataset_end_msg)
+
+    Log.info(main_config.log_msg.save_dataset_start_msg)
     write_parquet(df=dataset_df, path=main_config.get('raw_data_path'))
+    Log.info(main_config.log_msg.save_dataset_end_msg)
+
+    Log.info(main_config.log_msg.acquire_data_end_msg)
 
 
 if __name__ == '__main__':
