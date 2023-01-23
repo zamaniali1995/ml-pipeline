62706b11c1d352eb26724708668f00e11d0968cf
Ali <azamani1@ualberta.ca>
2023-01-23 15:35:13 -0700

    Add Log class

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,25 @@
 """
 Complete me
 """
+import logging
 import os
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from typing import Dict, List, Text
 
 import pandas as pd
+import structlog
 import yaml
+from munch import DefaultMunch
+
+struct_logger = structlog.getLogger(__name__)
+logging.basicConfig(
+    filename='log.log',
+    encoding='utf-8',
+    level=logging.DEBUG,
+    format='%(asctime)s %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p'
+)
 
 
 def write_parquet(df: pd.DataFrame, path: Path, engine: Text = 'pyarrow'):
@@ -40,13 +52,34 @@ def load_yaml_config(path: Path) -> Dict:
     """
     with open(path, 'r') as stream:
         try:
-            yaml_file = yaml.safe_load(stream)
+            yaml_obj = DefaultMunch.fromDict(yaml.safe_load(stream))
         except yaml.YAMLError as exc:
-            print(exc)
-    return yaml_file
+            Log.error(exc)
+    return yaml_obj
 
 
 def load_parquet(path: Path, engine='pyarrow') -> pd.DataFrame:
     """Complete me
     """
     return pd.read_parquet(path, engine=engine)
+
+
+class Log():
+    """Complete me
+    """
+
+    def error(self, msg: Text) -> None:
+        """Logs an input error message with the logger module
+
+        :param Text msg: input message with the recorded error
+        """
+        struct_logger.error(msg)
+        logging.error(msg=msg)
+
+    def info(msg: Text) -> None:
+        """Logs an input info message with the logger module
+
+        :param Text msg: input message with the recorded info
+        """
+        struct_logger.info(msg)
+        logging.info(msg=msg)
