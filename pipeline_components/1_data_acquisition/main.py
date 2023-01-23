3170c3aa21bcd3ac529b3979cb01ae8ccba0bc15
Ali <azamani1@ualberta.ca>
2023-01-23 16:04:05 -0700

    Add target_column and Modify log

--- pipeline_components/1_data_acquisition/main.py
+++ pipeline_components/1_data_acquisition/main.py
@@ -2,7 +2,7 @@
 The file should implement data acquisition pipeline.
 It should load the raw data from any data source and save it.
 """
-from typing import List
+from typing import List, Text
 
 import pandas as pd
 from sklearn import datasets
@@ -10,12 +10,12 @@ from sklearn import datasets
 from src.utils import Log, load_yaml_config, parse_arguments, write_parquet
 
 
-def load_data() -> pd.DataFrame:
+def load_data(target_column: Text) -> pd.DataFrame:
     """Complete me"""
     iris_dataset = datasets.load_iris()
     X = iris_dataset.data
     y = iris_dataset.target
-    y_df = pd.DataFrame(y, columns=['target'])
+    y_df = pd.DataFrame(y, columns=[target_column])
     X_df = pd.DataFrame(X, columns=iris_dataset.feature_names)
     iris_dataset_df = pd.concat([X_df, y_df], axis=1, join='inner')
     return iris_dataset_df
@@ -30,11 +30,11 @@ def main(arguments_list: List = None):
     Log.info(main_config.log_msg.acquire_data_start_msg)
 
     Log.info(main_config.log_msg.load_dataset_start_msg)
-    dataset_df = load_data()
+    dataset_df = load_data(target_column=main_config.target_column)
     Log.info(main_config.log_msg.load_dataset_end_msg)
 
     Log.info(main_config.log_msg.save_dataset_start_msg)
-    write_parquet(df=dataset_df, path=main_config.get('raw_data_path'))
+    write_parquet(df=dataset_df, path=main_config.path.raw_data_path)
     Log.info(main_config.log_msg.save_dataset_end_msg)
 
     Log.info(main_config.log_msg.acquire_data_end_msg)
