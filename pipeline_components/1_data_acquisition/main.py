531dcf67b71e19666454f7815467a0c1a66628ac
Feromond <Jacobpmish@gmail.com>
2023-01-25 14:21:08 -0700

    moved the load_data function to utils

--- pipeline_components/1_data_acquisition/main.py
+++ pipeline_components/1_data_acquisition/main.py
@@ -2,23 +2,11 @@
 The file should implement data acquisition pipeline.
 It should load the raw data from any data source and save it.
 """
-from typing import List, Text
-
-import pandas as pd
-from sklearn import datasets
+from typing import List
 
 from src.utils import Log, load_yaml, parse_arguments, write_parquet
+from src.data.acquisition.utils import load_data
 
-
-def load_data(target_column: Text) -> pd.DataFrame:
-    """Complete me"""
-    iris_dataset = datasets.load_iris()
-    X = iris_dataset.data
-    y = iris_dataset.target
-    y_df = pd.DataFrame(y, columns=[target_column])
-    X_df = pd.DataFrame(X, columns=iris_dataset.feature_names)
-    iris_dataset_df = pd.concat([X_df, y_df], axis=1, join='inner')
-    return iris_dataset_df
 
 
 def main(arguments_list: List = None):
--- src/data/acquisition/utils.py
+++ src/data/acquisition/utils.py
@@ -1,3 +1,29 @@
 """
 Complete me
 """
+from typing import List, Text
+
+import pandas as pd
+from sklearn import datasets
+
+
+def load_data(target_column: Text) -> pd.DataFrame:
+    """Loads data from the iris database into panda dataframes
+
+    Parameters
+    ----------
+    target_column : Text
+        The name of the column that is the target of prediction 
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe containing all of the data with the target column at the very end
+    """
+    iris_dataset = datasets.load_iris()
+    X = iris_dataset.data
+    y = iris_dataset.target
+    y_df = pd.DataFrame(y, columns=[target_column])
+    X_df = pd.DataFrame(X, columns=iris_dataset.feature_names)
+    iris_dataset_df = pd.concat([X_df, y_df], axis=1, join='inner')
+    return iris_dataset_df
